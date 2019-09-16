163434de0c061294c87ac1b7606e0688e60ac23f
jeffh <jeffh@nojapower.com.au>
2019-09-17 08:59:16 +1000

    Rebranding and version change

--- Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/openHistorian/openHistorianConsole/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
 
-[assembly: AssemblyVersion("2.6.30.0")]
+[assembly: AssemblyVersion("2.6.30.1")]
 
 // Informational attributes.
 
